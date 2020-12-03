58c470f2e9329993e97c42b7d2098fa1cdfcfec7
Shani-LevR <shani.levran@gmail.com>
2020-12-03 03:16:40 +0200

    Initialization of dota_app repository

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { fetchHeroes } from "./api/fetchData";
+import { createTeams } from "./api/util";
+import HerosTeam from "./components/HerosTeam";
 
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [teamDire, setTeamDire] = useState([]);
+  const [teamRadiant, setTeamRadiant] = useState([]);
+
+  const getHeros = async () => {
+    const heroes = await fetchHeroes();
+    setPlayers(heroes);
+  };
+
+  // Fetch data from API
+  useEffect(() => {
+    getHeros();
+  }, []);
+
+  // Select heroes for the match
+  useEffect(() => {
+    if (players.length > 0) {
+      console.log(players);
+      const [dire, radiant] = createTeams(players);
+      setTeamDire(dire);
+      setTeamRadiant(radiant);
+    }
+  }, [players]);
+
+  // Render "No data" while fetch is not finished, condition could be replaced by a 'isLoading' state
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="match__title">Match</h1>
+      <div className="match__teams">
+        {teamDire.length && teamRadiant.length ? (
+          <>
+            <HerosTeam team={teamDire} title="The Dire" />
+            <HerosTeam team={teamRadiant} title="The Radiant" />
+          </>
+        ) : (
+          <p>No data...</p>
+        )}
+      </div>
     </div>
   );
 }
